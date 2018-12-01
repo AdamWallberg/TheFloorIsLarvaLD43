27dad5aa0eaf74e53f943470bbce02567d8b48e4
Adam Wallberg <adamwallbergcontact@gmail.com>
2018-12-01 20:06:51 +0100

    Implement maggot chunk merging D:

--- TheFloorIsLarva/Assets/Scripts/MaggotChunk.cs
+++ TheFloorIsLarva/Assets/Scripts/MaggotChunk.cs
@@ -21,6 +21,9 @@ public class MaggotChunk : MonoBehaviour
 	[HideInInspector]public float _timeAtSplit;
 	public float _pushTimeThreshold = 1.0f;
 
+	// Merging
+	public bool _merging = false;
+
 	private void Awake()
 	{
 		_rb = GetComponent<Rigidbody2D>();
@@ -36,7 +39,7 @@ public class MaggotChunk : MonoBehaviour
 		transform.localScale = new Vector3(f, f, f);
 
 		// Die
-		if(_amount <= 0)
+		if(_amount <= 1.0f && !_merging)
 		{
 			Die();
 		}
@@ -44,34 +47,51 @@ public class MaggotChunk : MonoBehaviour
 		// Handle movement
 		MovementSizeReduction();
 
-		// Push away from other masses
-		PushAway();
+		// Interact with other chunks
+		Interact();
 	}
 
 	void MovementSizeReduction()
 	{
-		Vector3 posDelta = transform.position - _lastFramePos;
-		_amount -= posDelta.magnitude * _movementSizeReductionRate;
-		_lastFramePos = transform.position;
+		if(!_merging)
+		{
+			Vector3 posDelta = transform.position - _lastFramePos;
+			_amount -= posDelta.magnitude * _movementSizeReductionRate;
+			_lastFramePos = transform.position;
+		}
 	}
 
-	void PushAway()
+	void Interact()
 	{
-		if(Time.time - _timeAtSplit < _pushTimeThreshold)
+		foreach (var other in FindObjectsOfType<MaggotChunk>())
 		{
-			foreach(var mc in FindObjectsOfType<MaggotChunk>())
+			if (other == this)
+				continue;
+
+			Vector3 diff = transform.position - other.transform.position;
+
+			if (diff.magnitude <
+				other._coll.radius * other.transform.localScale.x +
+				_coll.radius * transform.localScale.x)
 			{
-				if (mc == this)
-					continue;
-
-				Vector3 diff = transform.position - mc.transform.position;
-
-				if (diff.magnitude < 
-					mc._coll.radius * mc.transform.localScale.x + 
-					_coll.radius * transform.localScale.x)
+				if (Time.time - _timeAtSplit < _pushTimeThreshold)
 				{
 					diff.Normalize();
 					_rb.AddForce(diff * Time.deltaTime * 100.0f);
+				}
+				else
+				{
+					diff.Normalize();
+					_rb.AddForce(-diff * Time.deltaTime * 50.0f);
+					if (!other._merging && !_merging)
+					{
+						other._merging = true;
+						_merging = true;
+						if(other._amount < _amount)
+							other.StartCoroutine(other.Merge(this));
+						else
+							StartCoroutine(Merge(other));
+					}
 				}
 			}
 		}
@@ -84,20 +104,39 @@ public class MaggotChunk : MonoBehaviour
 
 	public void Split()
 	{
-		if(_amount > _splitCost)
+		if(_amount > _splitCost + 1.0f)
 		{
 			MaggotChunk mm = Instantiate(_maggotMassPrefab);
 			mm.transform.position = transform.position;
-			mm._amount = 2;
+			mm._amount = _splitCost;
 			mm._timeAtSplit = _timeAtSplit = Time.time;
-			_amount -= 2;
-			_rb.AddForce(Vector2.up * 1.0f, ForceMode2D.Impulse);
-			mm._rb.AddForce(Vector2.down * 1.0f, ForceMode2D.Impulse);
+			_amount -= _splitCost;
+			_rb.AddForce(Vector2.up * 0.1f, ForceMode2D.Impulse);
+			mm._rb.AddForce(Vector2.down * 0.1f, ForceMode2D.Impulse);
 		}
 	}
 
 	public void Drag(Vector2 drag)
 	{
 		_rb.AddForce(drag * Time.deltaTime * 100.0f);
+	}
+
+	public IEnumerator Merge(MaggotChunk other)
+	{
+		yield return new WaitForSeconds(0.5f);
+
+		float newTotal = other._amount + _amount;
+		const float growRate = 2.0f;
+
+		while(other._amount < newTotal)
+		{
+			float delta = Time.deltaTime * growRate;
+			other._amount += delta;
+			_amount -= delta;
+			yield return null;
+		}
+		other._merging = false;
+		Destroy(gameObject);
+		enabled = false;
 	}
 }
