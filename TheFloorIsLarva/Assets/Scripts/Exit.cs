86954d80f9bb649c8bae305f8f944cd28e9d0a05
Adam Wallberg <adamwallbergcontact@gmail.com>
2018-12-02 12:18:10 +0100

    Add fading between rooms

--- TheFloorIsLarva/Assets/Scripts/Exit.cs
+++ TheFloorIsLarva/Assets/Scripts/Exit.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Exit : MonoBehaviour
 {
+	public Room _currentRoom;
 	public Room _nextRoom;
+	public Image _fadePanel;
+	public float _fadeTime = 0.5f;
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
@@ -16,12 +20,23 @@ public class Exit : MonoBehaviour
 
 	IEnumerator Transition(Transform player)
 	{
-		for(float f = 0.0f; f < 1.0f; f += Time.deltaTime)
+		for(float f = 0.0f; f < 1.0f; f += Time.deltaTime / _fadeTime)
 		{
-
+			_fadePanel.color = new Color(0f, 0f, 0f, f);
+			yield return null;
 		}
 
 		_nextRoom.SetActive(player);
+
+		for (float f = 1.0f; f > 0.0f; f -= Time.deltaTime / _fadeTime)
+		{
+			_fadePanel.color = new Color(0f, 0f, 0f, f);
+			yield return null;
+		}
+		_fadePanel.color = new Color(0f, 0f, 0f, 0f);
+
+		_currentRoom.gameObject.SetActive(false);
+
 		yield return null;
 	}
 }
